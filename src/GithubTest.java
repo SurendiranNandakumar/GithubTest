91a4d5da1b37c50c286ee99a084361750db5ee64
SurendiranNandakumar <surendiran.sureshraina@gmail.com>
2021-07-01 16:55:00 +0530

    We first push

--- src/GithubTest.java
+++ src/GithubTest.java
@@ -2,5 +2,9 @@
 public class GithubTest {
 public static void main(String[] args) {
 	System.out.println("Program");
+	System.out.println("Java");
+	System.out.println("Python");
+	System.out.println("Photoshop");
+
 }
 }
